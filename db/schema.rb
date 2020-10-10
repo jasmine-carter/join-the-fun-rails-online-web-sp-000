39c99240e0c53e48882ccdc8b682a1ed39859ab3
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-10 19:49:50 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212205318) do
+ActiveRecord::Schema.define(version: 20201010194910) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "ride_id"
+    t.integer  "taxi_id"
   end
 
   create_table "rides", force: :cascade do |t|
@@ -23,8 +25,10 @@ ActiveRecord::Schema.define(version: 20140212205318) do
   end
 
   create_table "taxis", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "passenger_id"
+    t.integer  "ride_id"
   end
 
 end
